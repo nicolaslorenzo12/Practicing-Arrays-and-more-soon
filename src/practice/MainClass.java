765484af3a245e12508da0dfa66d583b1683f4c3
Nicolas Lorenzo <nicolaslorenzohermosilla@gmail.com>
2021-09-26 19:58:11 +0200

    Putting the program in github

---
+++ src/practice/MainClass.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class MainClass {
+    public static void main(String[] args) {
+        MyArray myArray = new MyArray();
+        myArray.printingTheMenu();
+        myArray.printingTheOrder();
+    }
+}
---
+++ src/practice/MyArray.java
@@ -0,0 +1,45 @@
+package practice;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class MyArray {
+
+    private final String[] beers = {"Bush Beer","Duvel","La Chouffe", "Chimay"};
+    private ArrayList<String> beersOrdered = new ArrayList<>();
+    Scanner keyboard = new Scanner(System.in);
+
+    public void printingTheMenu(){
+        System.out.println("This is the menu: ");
+
+        for(int x = 0; x < beers.length; x++){
+            System.out.println(beers[x]);
+        }
+    }
+    public void printingTheOrder(){
+        System.out.println(orderingBeers());
+    }
+
+    private String orderingBeers(){
+        String beer = " ";
+        boolean beerFound = false;
+        while(!beer.equals("exit")){
+
+            System.out.print("What kind of beer do you want?: ");
+            beer = keyboard.nextLine();
+
+            for(int x = 0; x< beers.length; x++){
+                if(beer.equals(beers[x])){
+                    System.out.println(beers[x] + " has been added to your order");
+                    beersOrdered.add(beers[x]);
+                    x = beers.length;
+                    beerFound = true;
+                }
+            }
+            if(!beerFound && !beer.equals("exit")){
+                System.out.println("You should choose a beer of the menu");
+            }
+            beerFound = false;
+        }
+        return "Your order is: " + beersOrdered;
+    }
+}
